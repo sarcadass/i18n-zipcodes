67c46360d791da39dd2297a11c03526064d37c53
Benjamin Blonde <sarcadass@gmail.com>
2020-02-22 15:24:59 +0800

    Prefix lib dist with info string header

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var gulp = require('gulp'),
-	browserify = require('browserify'),
-	uglify = require('gulp-uglify'),
-	source = require('vinyl-source-stream'),
-	rename = require('gulp-rename');
+const gulp = require('gulp')
+const browserify = require('browserify')
+const uglify = require('gulp-uglify')
+const source = require('vinyl-source-stream')
+const rename = require('gulp-rename')
+const header = require('gulp-header')
+const appVersion = require('./package.json').version
+const libHeader = `/*! i18n-zipcodes v${appVersion} - https://github.com/sarcadass/i18n-zipcodes */\n`
 
 gulp.task('build', function() {
 	return browserify({entries: './standalone.js'}).bundle()
 		.pipe(source('./i18n-zipcodes.js'))
+		.pipe(header(libHeader))
 		.pipe(gulp.dest('./dist/'));
 });
 
@@ -16,6 +20,7 @@ gulp.task('buildMin', gulp.series('build', function() {
 	return gulp.src('./dist/i18n-zipcodes.js')
 		.pipe(rename({ suffix: ".min" }))
 		.pipe(uglify())
+		.pipe(header(libHeader))
 		.pipe(gulp.dest('./dist'));
 }));
 
